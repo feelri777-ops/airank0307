e223fe28d512db48b07d60d5569fd5ae3005442b
feekri <feekri@feelri.local>
2026-03-18 23:18:41 +0900

    UI: Update bookmark toggle with Phosphor icons and reduce size

--- src/components/icons/PhosphorIcons.jsx
+++ src/components/icons/PhosphorIcons.jsx
@@ -13,3 +13,17 @@ export const YoutubeLogoFill = ({ size = 18, color = "currentColor", ...props })
     <path d="M234.33,69.52a24,24,0,0,0-14.49-16.4C185.56,39.88,131,40,128,40s-57.56-.12-91.84,13.12a24,24,0,0,0-14.49,16.4C19.08,79.5,16,97.74,16,128s3.08,48.5,5.67,58.48a24,24,0,0,0,14.49,16.41C69,215.56,120.4,216,127.34,216h1.32c6.94,0,58.37-.44,91.18-13.11a24,24,0,0,0,14.49-16.41c2.59-10,5.67-28.22,5.67-58.48S236.92,79.5,234.33,69.52Zm-73.74,65-40,28A8,8,0,0,1,108,156V100a8,8,0,0,1,12.59-6.55l40,28a8,8,0,0,1,0,13.1Z"/>
   </svg>
 );
+
+export const BookmarkSimple = ({ size = 20, color = "currentColor", ...props }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width={size} height={size} {...props}>
+    <rect width="256" height="256" fill="none"/>
+    <path d="M192,224l-64-40L64,224V48a8,8,0,0,1,8-8H184a8,8,0,0,1,8,8Z" fill="none" stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+  </svg>
+);
+
+export const BookmarkSimpleFill = ({ size = 20, color = "currentColor", ...props }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width={size} height={size} fill={color} {...props}>
+    <rect width="256" height="256" fill="none"/>
+    <path d="M184,32H72A16,16,0,0,0,56,48V224a8,8,0,0,0,12.24,6.78L128,193.43l59.77,37.35A8,8,0,0,0,200,224V48A16,16,0,0,0,184,32Z"/>
+  </svg>
+);
